567a989ee943c948279ed04905cc8d34303c3f17
Andrew Klenner <atklenner@gmail.com>
2023-06-01 15:42:08 -0700

    misc edits, none of them that important

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,11 @@
-export const SITE = {
-  title: 'Astro Blog with Netlify CMS',
+interface SiteDetails {
+  title: String;
+  baseURL: String;
+  description: String;
+}
+
+export const SITE: SiteDetails = {
+  title: "Astro Blog with Decap CMS",
+  baseURL: "https://astroplusdecap.netlify.app",
+  description: "A demo blog build with Astro and Decap CMS by Andrew Klenner",
 };
